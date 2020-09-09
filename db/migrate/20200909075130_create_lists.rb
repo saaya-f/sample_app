3a6ff9316c1e05e6540ae99d26324d632509d1f2
EC2 Default User <ec2-user@ip-172-31-21-26.ec2.internal>
2020-09-09 09:00:04 +0000

    [Finish]2-3

--- db/migrate/20200909075130_create_lists.rb
+++ db/migrate/20200909075130_create_lists.rb
@@ -1,8 +1,11 @@
 class CreateLists < ActiveRecord::Migration[5.2]
   def change
     create_table :lists do |t|
-
+      t.string :title
+      t.string :body
       t.timestamps
+      # t.timestamps ：このテーブルに書込・修正した時に
+      # そのタイムスタンプを保存しておくためのカラム
     end
   end
 end
