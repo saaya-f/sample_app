99945deca38c71a8ed57e9b173c2d8c0300c1bda
EC2 Default User <ec2-user@ip-172-31-21-26.ec2.internal>
2020-09-10 09:19:29 +0000

    [Finish]2-4

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,5 +1,21 @@
 class TodolistsController < ApplicationController
   def new
-    @List = List.new
+    @list = List.new
+  end
+  def create
+    # データを新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    # データをDBに保存するためのsaveメソッド実行
+    list.save
+    # トップ画面へリダイレクト
+    redirect_to '/top'
+  end
+
+  private
+  # ストロングパラメータ：マスアサインメント傍若生を防ぐ境界線
+  # ここから下の行はメソッド呼出し制限をかけられ,
+  # アクションとして認識されない
+  def list_params
+    params.require(:list).permit(:title, :body)
   end
 end
