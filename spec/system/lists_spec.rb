aa7a3cd23c472990a76d5f17241e735ce9853bf0
EC2 Default User <omigun3@icloud.com>
2021-01-17 08:18:51 +0000

    [Add]controller_todolists_new_path

--- spec/system/lists_spec.rb
+++ spec/system/lists_spec.rb
@@ -17,4 +17,35 @@ describe "投稿のテスト" do
       end
     end
   end
+
+  descibe "投稿画面(todolists_new_path)のテスト" do
+    before do
+      visit todolists_new_path
+    end
+    context "表示の確認" do
+      it 'todolists_new_pathが"/todolists/new"であるか' do
+        expect(current_path).to eq('/todolists/new')
+      end
+      it "投稿ボタンが表示されているか" do
+        expect(page).to have_button '投稿'
+      end
+    end
+    context "投稿処理のテスト" do
+      it "投稿後のリダイレクト先は正しいか" do
+        fill_in 'list[title]', with: Fakker::Lorem.characters(number:5)
+        fill_in 'list[body]', with: Fakker::Lorem.characters(number:20)
+        click_button is '投稿'
+        expect(page).to have_current_path todolist_path(List.last)
+      end
+    end
+  end
+
+  descibe "一覧画面(todolists_path)のテスト" do
+  end
+
+  descibe "詳細画面(todolist_path)のテスト" do
+  end
+
+  descibe "編集画面(edit_todolist_path)のテスト" do
+  end
 end
